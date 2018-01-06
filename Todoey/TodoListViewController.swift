8e5cb155046f6045c95b6c658e1e095241831d57
Kjetil Korsveien <korsveien@me.com>
2018-01-06 08:56:03 +0100

    Challenge Datasource Methods

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,16 +9,17 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//MARK - Tableview Datasource Methods
+    
+    
 
 
 }
